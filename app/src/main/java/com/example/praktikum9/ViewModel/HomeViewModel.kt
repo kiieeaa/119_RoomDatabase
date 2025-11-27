c9f624e916c831127ff20a7157ef895930b8b610
kiieeaa <kiyaadinda5@gmail.com>
2025-11-27 17:10:24 +0700

    Mengambil data siswa dari repository lalu mengubahnya menjadi HomeUiState untuk ditampilkan di UI

---
+++ app/src/main/java/com/example/praktikum9/ViewModel/HomeViewModel.kt
@@ -0,0 +1,15 @@
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.praktikum9.repositori.RepositoriSiswa
+import com.example.praktikum9.room.Siswa
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
+
+class HomeViewModel(private val repositoriSiswa: RepositoriSiswa) : ViewModel() {
+
+    companion object {
+        private const val TIMEOUT_MILLIS = 5_000L
+    }
