8141c1dfc011befe785ec0c82885f31d50d801e1
kiieeaa <kiyaadinda5@gmail.com>
2025-11-27 21:29:07 +0700

    Mendefinisikan destinasi halaman utama (Home) dengan route "home" dan judul dari resource app_name.

---
+++ app/src/main/java/com/example/praktikum9/view/route/DestinasiHome.kt
@@ -0,0 +1,11 @@
+package com.example.praktikum9.view.route
+
+
+
+import com.example.praktikum9.R
+
+object DestinasiHome : DestinasiNavigasi {
+    override val route = "home"
+
+    override val titleRes = R.string.app_name
+}
