6061d4eb549f53a11948f8471a6c669585e1a95e
kiieeaa <kiyaadinda5@gmail.com>
2025-11-27 17:00:56 +0700

    Membuat instance database Room bernama "siswa_database"

--- app/src/main/java/com/example/praktikum9/room/DatabaseSiswa.kt
+++ app/src/main/java/com/example/praktikum9/room/DatabaseSiswa.kt
@@ -13,5 +13,21 @@ abstract class DatabaseSiswa : RoomDatabase() {
     companion object {
         @Volatile
         private var Instance: DatabaseSiswa? = null
+
+        fun getDatabase(context: Context): DatabaseSiswa {
+            return (Instance ?: synchronized(this) {
+
+
+                Room.databaseBuilder(
+                    context,
+                    DatabaseSiswa::class.java,
+                    "siswa_database"
+                )
+                    .build()
+                    .also { Instance = it }
+            })
+        }
+    }
 }
+
 
