0233c7971c7fae1db1df2c53c2deb841d508ebaf
kiieeaa <kiyaadinda5@gmail.com>
2025-11-27 17:08:29 +0700

    Menginisialisasi container ketika aplikasi pertama kali dijalankan

--- app/src/main/java/com/example/praktikum9/repositori/ContainerApp.kt
+++ app/src/main/java/com/example/praktikum9/repositori/ContainerApp.kt
@@ -22,3 +22,9 @@ class AplikasiSiswa : Application() {
 
 
     lateinit var container: ContainerApp
+    override fun onCreate() {
+        super.onCreate()
+
+        container = ContainerDataApp(this)
+    }
+}
