916f76dda4770e1bfc1af8c2a2cf1775e533d1fa
kiieeaa <kiyaadinda5@gmail.com>
2025-12-04 16:53:13 +0700

    menambahkan 3 komponen

--- app/src/main/java/com/example/praktikum9/repositori/RepositoriSiswa.kt
+++ app/src/main/java/com/example/praktikum9/repositori/RepositoriSiswa.kt
@@ -1,20 +1,31 @@
 package com.example.praktikum9.repositori
 
+
 import com.example.praktikum9.room.Siswa
 import com.example.praktikum9.room.SiswaDao
 import kotlinx.coroutines.flow.Flow
 
 interface RepositoriSiswa {
-
-
     fun getAllSiswaStream(): Flow<List<Siswa>>
     suspend fun insertSiswa(siswa: Siswa)
+
+    //tambah
+
+    fun getSiswaStream(id: Int): Flow<Siswa?>
+
+    suspend fun deleteSiswa(siswa: Siswa)
+
+    suspend fun updateSiswa(siswa: Siswa)
 }
 
 class OfflineRepositoriSiswa(
     private val siswaDao: SiswaDao
-) : RepositoriSiswa {
-
+): RepositoriSiswa {
     override fun getAllSiswaStream(): Flow<List<Siswa>> = siswaDao.getAllSiswa()
     override suspend fun insertSiswa(siswa: Siswa) = siswaDao.insert(siswa)
+
+    //tambah
+    override fun getSiswaStream(id: Int): Flow<Siswa?> = siswaDao.getSiswa(id)
+    override suspend fun deleteSiswa(siswa: Siswa) = siswaDao.delete(siswa)
+    override suspend fun updateSiswa(siswa: Siswa) = siswaDao.update(siswa)
 }
