e58a7a0b46decdab9d9c0240a4a4099868b9a631
kiieeaa <kiyaadinda5@gmail.com>
2025-12-04 18:51:10 +0700

    Mengambil data siswa berdasarkan ID dan mengembalikannya sebagai Flow untuk pemantauan realtime

--- app/src/main/java/com/example/praktikum9/room/SiswaDao.kt
+++ app/src/main/java/com/example/praktikum9/room/SiswaDao.kt
@@ -1,18 +1,24 @@
 package com.example.praktikum9.room
 
 import androidx.room.Dao
+import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
+import androidx.room.Update
 import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface SiswaDao {
-
 
     @Query("SELECT * from tblSiswa ORDER BY nama ASC")
     fun getAllSiswa(): Flow<List<Siswa>>
 
     @Insert(onConflict = OnConflictStrategy.IGNORE)
     suspend fun insert(siswa: Siswa)
+
+    //edit : tambah 3 fungsi berikut
+    @Query("SELECT * from tblSiswa WHERE id = :id")
+    fun getSiswa(id: Int): Flow<Siswa>
+
 }
